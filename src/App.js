9f77c4818174a82aa4160e74f7ded6004beaabcb
kanishk44 <ks468@njit.edu>
2023-04-09 20:09:47 -0400

    Fetched data from open weather API, and created weather component and styled it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "semantic-ui-css/semantic.min.css";
+import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
+import { Dimmer, Loader } from "semantic-ui-react";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, long]);
+
+  return (
+    <div className="App">
+      {typeof data.main != undefined ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default App;
